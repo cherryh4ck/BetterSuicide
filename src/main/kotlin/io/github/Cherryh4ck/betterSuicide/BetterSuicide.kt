f78ff5c0c2431db233b03bdc3637915465421207
Cherryh4ck <tomas1234567891016@gmail.com>
2026-07-30 19:08:32 -0300

    Add main command

--- src/main/kotlin/io/github/Cherryh4ck/betterSuicide/BetterSuicide.kt
+++ src/main/kotlin/io/github/Cherryh4ck/betterSuicide/BetterSuicide.kt
@@ -1,6 +1,8 @@
 package io.github.Cherryh4ck.betterSuicide
 
 import net.kyori.adventure.text.minimessage.MiniMessage
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerCommandPreprocessEvent
@@ -15,6 +17,7 @@ class BetterSuicide : JavaPlugin(), Listener {
     var cooldownMessage : Boolean = false
     var noPermissions : String = ""
     var onCooldown : String = ""
+    var pluginPrefix : String = ""
 
     override fun onEnable() {
         saveDefaultConfig()
@@ -26,12 +29,15 @@ class BetterSuicide : JavaPlugin(), Listener {
 
         val suicide = getCommand("suicide")
         val kill = getCommand("kill")
+        val bettersuicide = getCommand("bettersuicide")
 
         suicide?.setExecutor(Suicide(this))
         kill?.setExecutor(Suicide(this))
+        bettersuicide?.setExecutor(this)
 
         suicide?.tabCompleter = Suicide(this)
         kill?.tabCompleter = Suicide(this)
+        bettersuicide?.tabCompleter = this
 
         server.pluginManager.registerEvents(this, this)
     }
@@ -41,6 +47,7 @@ class BetterSuicide : JavaPlugin(), Listener {
     }
 
     fun loadConfig(){
+        pluginPrefix = config.getString("plugin-prefix") ?: ""
         redirectKill = config.getBoolean("redirect-kill-for-ops")
         usePermissions = config.getBoolean("use-permission")
         enableCooldown = config.getBoolean("enable-cooldown")
@@ -48,6 +55,11 @@ class BetterSuicide : JavaPlugin(), Listener {
         cooldownMessage = config.getBoolean("enable-cooldown-message")
         noPermissions = config.getString("no-permissions") ?: ""
         onCooldown = config.getString("cooldown-active") ?: ""
+    }
+
+    fun reloadPluginConfig(){
+        reloadConfig()
+        loadConfig()
     }
 
     @EventHandler
@@ -58,5 +70,37 @@ class BetterSuicide : JavaPlugin(), Listener {
         if (message.startsWith("/kill") && player.isOp && redirectKill) {
             event.message = message.replaceFirst("/kill", "/minecraft:kill")
         }
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (args.isEmpty()) {
+            sender.sendMessage(minimessage.deserialize("$pluginPrefix <gold><b>${this.pluginMeta.version}"))
+            return true
+        }
+
+        when (args[0].lowercase()) {
+            "reload" -> {
+                reloadPluginConfig()
+                sender.sendMessage(minimessage.deserialize("$pluginPrefix <gold>Reloaded!"))
+                return true
+            }
+            else -> {
+                sender.sendMessage(minimessage.deserialize("$pluginPrefix <red>Not a valid command."))
+                return true
+            }
+        }
+    }
+
+    override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<out String>): List<String>? {
+        val completions = mutableListOf<String>()
+        if (args.size == 1) {
+            val subs = listOf("reload")
+            for (s in subs) {
+                if (s.startsWith(args[0].lowercase())) {
+                    completions.add(s)
+                }
+            }
+        }
+        return completions
     }
 }
